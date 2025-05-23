35d7ea8029a06b4411577e62f647156cfd50406d
Brendan Flatley <brendanflatley@Mac-361.lan>
2025-05-22 20:38:00 -0700

    Added manager info for me and picture for Minkoff

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -8,10 +8,12 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 /*   STEP 2   */
 export const homepageText = `
   <p>The unofficial page to see Brendan Flatley & the pope's fav christians (new pope, whaddup) plus nine other nerds' fantasy football history since the inception of the Alpha Beta Dynasty League. Or, since 2021 because I think we were on ESPN or something before that. Thank you Sleeper for the sick, free API.</p>
-  <p>The coolest thing so far is the rival page under league info where you can see total wins/losses against another manager, any trades between the two of you, and some overall stats. Looking to add more cool things soon, and there will be power rankings under this description when the season starts.</p>
+  <p>The coolest thing so far is the rivalry page under League Info where you can see total wins/losses against another manager, any trades between the two of you, and some overall stats. Looking to add more cool things soon, and there will be power rankings under this description when the season starts.</p>
   <p>Any suggestions for the website can be sent to my secretary and current commish, John Watts.</p>
   <p>Go Rams</p>
 `;
+
+homepageText.style.textAlign = 'center';
 
 /*   STEP 3   */
 /*
@@ -26,25 +28,25 @@ export const managers = [
     {
     //  "roster": 3,  // (DEPRECATED! Don't use this anymore) ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID": "469686492965367808",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
-      "name": "Brendan Flatley",
+      "name": "bflats7",
     //  "tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Los Angeles, CA", // (optional)
-      "bio": "Cool as fuck, loves the game, and is a great guy. Also, I have a great sense of humor and am very humble",
-      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+      "bio": "Cool as fuck, loves the game, and is a great guy. Driftin and griftin, baby",
+      "photo": "/managers/me_mufucka.jpeg", // square ratio recommended (no larger than 500x500)
     //  "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "lar", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
-        name: "Rival", // Can be anything (usually your rival's name)
+        name: "Aron Minkoff", // Can be anything (usually your rival's name)
         link: 6, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        image: "/managers/minkoff.jpeg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 9493, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-      "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
+      "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Horde RBs and win when it matters", // (optional)
       "tradingScale": 7, // 1 - 10 (optional)
-      "preferredContact": "Carrier Pigeon",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+      "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
   ]
   
